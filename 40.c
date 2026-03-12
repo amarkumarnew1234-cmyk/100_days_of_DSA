4883b1d2f5e407daa339fef2f1b1de7c7b73beee
amarkumarnew1234-cmyk <amarkumarnew1234@gmail.com>
2026-03-12 21:12:57 +0530

    Add files via upload

---
+++ 40.c
@@ -0,0 +1,43 @@
+/*Implement Heap Sort using a Max Heap to sort an array in ascending order. 
+First build a max heap, then repeatedly extract the maximum element and place it at the end of the array.*/
+#include<stdio.h>
+void heapify(int a[],int n,int i)
+{
+    int largest=i,left=2*i+1,right=2*i+2,temp;
+    if(left<n && a[left]>a[largest])
+        largest=left;
+    if(right<n && a[right]>a[largest])
+        largest=right;
+    if(largest!=i)
+    {
+        temp=a[i];
+        a[i]=a[largest];
+        a[largest]=temp;
+        heapify(a,n,largest);
+    }
+}
+void heapsort(int a[],int n)
+{
+    int i,temp;
+    for(i=n/2-1;i>=0;i--)
+        heapify(a,n,i);
+    for(i=n-1;i>0;i--)
+    {
+        temp=a[0];
+        a[0]=a[i];
+        a[i]=temp;
+        heapify(a,i,0);
+    }
+}
+int main()
+{
+    int n,i,a[100];
+    printf("Enter no.of elements:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    heapsort(a,n);
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
+    return 0;
+}
