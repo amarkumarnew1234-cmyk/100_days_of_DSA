07986618c67f0fa7811d489053ed31e30b9e01a1
amarkumarnew1234-cmyk <amarkumarnew1234@gmail.com>
2026-04-26 21:42:11 +0530

    Add files via upload

---
+++ 85.c
@@ -0,0 +1,73 @@
+/*Problem: Implement Merge Sort - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array or search result
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64*/
+#include<stdio.h>
+void merge(int a[],int l,int m,int r){
+    int n1=m-l+1;
+    int n2=r-m;
+    int L[n1],R[n2];
+    for(int i=0;i<n1;i++){
+        L[i]=a[l+i];
+    }
+    for(int j=0;j<n2;j++){
+        R[j]=a[m+1+j];
+    }
+    int i=0,j=0,k=l;
+    while(i<n1&&j<n2){
+        if(L[i]<=R[j]){
+            a[k]=L[i];
+            i++;
+        }else{
+            a[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        a[k]=L[i];
+        i++;
+        k++;
+    }
+    while(j<n2){
+        a[k]=R[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int a[],int l,int r){
+    if(l<r){
+        int m=l+(r-l)/2;
+        mergeSort(a,l,m);
+        mergeSort(a,m+1,r);
+        merge(a,l,m,r);
+    }
+}
+int main(){
+    int n;
+    printf("Enter no.of elements:");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter elements:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    mergeSort(a,0,n-1);
+    printf("Sorted array:");
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
